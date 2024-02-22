23f0815c24aa98014f6463bbcf1092eef2d3d895
Maciej <msypniewski511@gmail.com>
2024-02-22 08:59:21 +0000

    Test pronto

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_15_074236) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_20_115736) do
   create_table "composers", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -27,6 +27,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_15_074236) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["work_id"], name: "index_editions_on_work_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "works", force: :cascade do |t|
